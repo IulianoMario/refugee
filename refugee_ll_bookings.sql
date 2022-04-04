164adb6d8156b788c995c151fae595e228db3dc0
Mario Iuliano <100870427+IulianoMario@users.noreply.github.com>
2022-04-04 09:02:21 +0200

    Update refugee_ll_bookings.sql

--- refugee_ll_bookings.sql
+++ refugee_ll_bookings.sql
@@ -1,3 +1,7 @@
+{{ config(
+    alias="refugee_ll_bookings"
+) }}
+
 WITH
   deals AS (
   SELECT
@@ -142,8 +146,7 @@ WITH
     _ll_maximum_rent_duration_possible_,
     te_number_of_people_in_the_group_keep_updated,
     te_number_of_children_under_18_
-  FROM
-    `data-warehouse-229515.staging.pipedrive_refugee_deals` a
+  from {{ ref('stg_pipedrive_refugee__refugee_deals') }} a
   LEFT JOIN
     UNNEST(a.person_id.email) AS email
   WITH
@@ -176,7 +179,6 @@ WITH
     core_ls.is_published,
     core_ls.is_deleted,
     core_ls.created_at,
-    --date_diff(current_date(), date(core_ls.created_at), day) as prd_nights_from_won_date, -- why do we need this one?
     DATE(core_ls.available_from) AS available_from,
     DATE(core_ls.available_to) AS available_to,
     core_ls.beds,
@@ -186,15 +188,15 @@ WITH
     core_ls.accommodates,
     core_ls.area,
     core_ls.next_available_date
-  FROM `data-warehouse-229515.wunderflats_core_refugee.bookings` rfg_bk
-  LEFT JOIN `data-warehouse-229515.wunderflats_core.listings` core_ls
+  from {{ ref('core_refugee__bookings') }} rfg_bk
+  left join {{ ref('core__listings') }}  core_ls
   ON rfg_bk.listing_id = core_ls.listing_id
   WHERE core_ls.listing_id IS NOT NULL 
   ),rfg_listing_request AS (
   SELECT
     DISTINCT listing_id,
     pets_allowed
-  FROM `data-warehouse-229515.wunderflats_core_refugee.listing_requests`
+  from {{ ref('core_refugee__listing_requests') }} 
 ),
     bookings_refugee AS (
     SELECT
@@ -206,8 +208,6 @@ WITH
         rfg_bk.booked_to, 
         rfg_bk.is_canceled,
         rfg_bk.listing_id,
-        -- Does it matter to have a listing created_at column ? Shall we not have the booking created at date? 
-        --core_bk.created_at as listing_created_at,
         IF(rfg_lis.is_online is null, core_bk.is_online, rfg_lis.is_online) as is_online,
         IF(rfg_lis.is_bookable is null, core_bk.is_bookable, rfg_lis.is_bookable) as is_bookable,
         IF(rfg_lis.is_published is null, core_bk.is_published, rfg_lis.is_published) as is_published,
@@ -224,45 +224,32 @@ WITH
         IF(rfg_lis.area is null, core_bk.area, rfg_lis.area) as area, 
         IF(rfg_lis.next_available_date is null, core_bk.next_available_date, rfg_lis.next_available_date) as next_available_date,
         cast(rfg_lr.pets_allowed as BOOL) as pets_allowed
-    FROM `data-warehouse-229515.wunderflats_core_refugee.bookings` rfg_bk
+    FROM {{ ref('core_refugee__bookings') }} rfg_bk
     LEFT JOIN bookings_core core_bk
     ON rfg_bk.booking_id = core_bk.booking_id
-    LEFT JOIN `data-warehouse-229515.wunderflats_core_refugee.listings` rfg_lis
+    LEFT JOIN {{ ref('core_refugee__listings') }} rfg_lis
     ON rfg_lis.listing_id = rfg_bk.listing_id
     LEFT JOIN rfg_listing_request rfg_lr
     ON rfg_lis.listing_id = rfg_lr.listing_id
-    left join `data-warehouse-229515.wunderflats_core_refugee.landlords` rfg_ll
+    left join {{ ref('core_refugee__landlords') }}  rfg_ll
     on rfg_ll.landlord_id = rfg_bk.landlord_id
 ), 
     pipedrive_bookings as (
         Select 
         id, 
         email, 
-        won_time as created_at, 
-        --booked_from not available, 
-        --booked_to not available, 
-        --is_cancelled not available, 
-        --listing_id not available,
-        --is_online not available, 
-        --is_bookable not available, 
-        --is_published not available,   
+        won_time as created_at,  
         deleted as is_deleted,
-         --listing_available_from not available,
-        --listing_available_to not available, 
         nights_per_rent_duration,
         min_nights_per_rent_duration,
         max_nights_per_rent_duration, 
         ll_max_rent_duration, 
         nights_from_won_date as nights_spent,
         ll_rent_per_person_numeric as price,
-        ll_rent_per_person,
-        --is_bookable not available, 
-        --is_published not available,   
+        ll_rent_per_person,  
         _ll_beds_available_in_total_ as beds,
         ll_slots_available_edit_team_1 as beds_edited_team1, 
         city,
-        --area not available, 
-        --next_available_date not available, 
         ll_pets_allowed_bool as pets_allowed
         from won_pipedrive_deals
     ), full_join_tab as (
@@ -271,95 +258,74 @@ WITH
              when pd.id is not null and prd.booking_id is null then 'in_pipedrive_only'
              when pd.id is null and prd.booking_id is not null then 'in_product_only'
         end as source_flag,
-        
         case when pd.id is not null and prd.booking_id is not null then prd.booking_id
              when pd.id is null and prd.booking_id is not null then prd.booking_id 
              when pd.id is not null and prd.booking_id is null then pd.id
              end as distinct_bk_id, 
-
         pd.id as pd_bk_id, 
         prd.booking_id as prd_bk_id,
-        
         case when pd.email is not null and prd.email  is not null then prd.email 
             when pd.email is null and prd.email  is not null then prd.email  
             when pd.email is not null and prd.email  is null then pd.email
             end as email, 
         pd.email as pd_email, 
         prd.email as prd_email,
-        
-        prd.landlord_id, 
-
+        prd.landlord_id,
         case when  pd.created_at is not null and  date(prd.created_at) is not null then date(prd.created_at) 
              when  pd.created_at is null and  date(prd.created_at) is not null then date(prd.created_at) 
              when  pd.created_at is not null and  date(prd.created_at) is null then pd.created_at
         end as created_at, 
         pd.created_at as pd_created_at, 
         prd.created_at as prd_created_at,
-
         case when pd.id is not null and prd.booking_id is not null then prd.booked_from else prd.booked_from end as booked_from, 
         case when pd.id is not null and prd.booking_id is not null then prd.booked_to else prd.booked_from end as booked_to, 
-
-
         case when pd.city is not null and prd.city is not null then prd.city 
              when pd.city is null and prd.city  is not null then prd.city 
              when pd.city is not null and prd.city  is null then pd.city
         end as city, 
-
         case when pd.max_nights_per_rent_duration is null then prd.nights_per_rent_duration
              when pd.max_nights_per_rent_duration is not null then pd.max_nights_per_rent_duration end max_nights_per_rent_duration,
-
         case when pd.min_nights_per_rent_duration is null then prd.nights_per_rent_duration
              when pd.min_nights_per_rent_duration is not null then pd.min_nights_per_rent_duration end min_nights_per_rent_duration,
-
         case when pd.nights_per_rent_duration is null then prd.nights_per_rent_duration
              when pd.nights_per_rent_duration is not null then pd.nights_per_rent_duration end nights_per_rent_duration,
-        
         case when pd.nights_per_rent_duration is null then prd.nights_per_rent_duration end as prd_nights_rent_duration,
-        
         ll_max_rent_duration as pd_max_rent_duration_raw,
         case when ll_max_rent_duration is null and prd.nights_per_rent_duration < 30 then "<1 mo"
              when ll_max_rent_duration is null and prd.nights_per_rent_duration = 30 then "1 mo"
              when ll_max_rent_duration is null and (prd.nights_per_rent_duration >30 and prd.nights_per_rent_duration < 90 ) then "1-3 mo"
              when ll_max_rent_duration is null and (prd.nights_per_rent_duration > 90 and prd.nights_per_rent_duration < 180) then "3-6 mo"
              when ll_max_rent_duration is null and prd.nights_per_rent_duration >= 180 then ">6 mo"
-             when ll_max_rent_duration is not null then ll_max_rent_duration end as max_rent_duration_cat,
-
+             when ll_max_rent_duration is not null then ll_max_rent_duration end as max_rent_duration_categorical,
         case when pd.nights_spent is not null and prd.nights_spent is not null then prd.nights_spent
              when pd.nights_spent is null and prd.nights_spent is not null then prd.nights_spent 
              when pd.nights_spent is not null and prd.nights_spent is null then pd.nights_spent
              end as nights_spent,
-
         case when pd.beds_edited_team1 is not null and prd.beds is not null then prd.beds  
             when pd.beds_edited_team1 is null and prd.beds  is not null then prd.beds  
             when pd.beds_edited_team1 is not null and prd.beds  is null then pd.beds_edited_team1 
             end as pd_beds_edited_team1,
-
         case when pd.beds is not null and prd.beds is not null then prd.beds 
             when pd.beds is null and prd.beds is not null then prd.beds 
             when pd.beds is not null and prd.beds is null then pd.beds 
             end as beds,
-        
         case when pd.price is not null and prd.price is not null then prd.price 
              when pd.price is null and prd.price is not null then prd.price 
              when pd.price is not null and prd.price is null then pd.price 
              end as price,
-        
         pd.ll_rent_per_person as pd_ll_rent_per_person,
         case when prd.price = 0 then 'for_free'
              when prd.price < 250 then '<250 mo' 
              when prd.price >=250 and prd.price < 500 then '250-500 mo' 
              when prd.price >=500 then '>500 mo'
-             else null end as prd_rent_per_person_cat,
-
+             else null end as prd_rent_per_person_categorical,
         case when pd.email is not null and prd.email is not null then prd.rooms
              when pd.email is null and prd.email is not null then prd.rooms 
              end as rooms, 
-
         case when pd.pets_allowed is not null and prd.pets_allowed is not null then prd.pets_allowed 
             when pd.pets_allowed is null and prd.pets_allowed is not null then prd.pets_allowed 
             when pd.pets_allowed is not null and prd.pets_allowed is null then pd.pets_allowed 
             end as pets_allowed,
-        
         case when pd.id is not null or pd.id is null then prd.accommodates end as accommodates, 
         case when pd.id is not null or pd.id is null then prd.is_bookable end as is_bookable, 
         case when pd.id is not null or pd.id is null then prd.is_online end as is_online, 
@@ -373,8 +339,8 @@ WITH
             Select 
             source_flag, 
             distinct_bk_id, 
-            pd_bk_id, 
-            prd_bk_id, 
+            pd_bk_id as pd_id, 
+            prd_bk_id as prd_bookingid, 
             email, 
             pd_email, 
             prd_email, 
@@ -382,15 +348,15 @@ WITH
             created_at,
             pd_created_at, 
             prd_created_at, 
-            booked_from,
-            booked_to,
+            booked_from as prd_booked_from,
+            booked_to as  prd_booked_to,
             city, 
             max_nights_per_rent_duration, 
             min_nights_per_rent_duration, 
             nights_per_rent_duration, 
             prd_nights_rent_duration, 
             pd_max_rent_duration_raw, 
-            max_rent_duration_cat, 
+            max_rent_duration_categorical, 
             nights_spent, 
             pd_beds_edited_team1, 
             beds, 
@@ -398,9 +364,9 @@ WITH
             case when pd_ll_rent_per_person is null and prd_rent_per_person_cat is not null then prd_rent_per_person_cat
                  when pd_ll_rent_per_person is not null and prd_rent_per_person_cat is null then pd_ll_rent_per_person
                  when pd_ll_rent_per_person is not null and prd_rent_per_person_cat is not null then prd_rent_per_person_cat 
-                 end as rent_per_person_cat,
+                 end as rent_per_person_categorical,
             pd_ll_rent_per_person, 
-            prd_rent_per_person_cat, 
+            prd_rent_per_person_categorical, 
             rooms, 
             accommodates, 
             pets_allowed,
